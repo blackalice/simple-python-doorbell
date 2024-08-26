9f1bf7a6ec13988b1a184f4f1e3397d207a160ad
Stuart Foy <stu@rtfoy.co.uk>
2024-08-26 14:38:15 +0100

    Update doorbell-server.py

--- doorbell-server.py
+++ doorbell-server.py
@@ -19,11 +19,11 @@ def ring():
     if request.headers.get('X-API-Key') != API_KEY:
         abort(401)  # Unauthorized
 
-    toast = Notification(app_id= config['DETAILS']['appid'],
-                         title= config['DETAILS']['title'],
-                         msg= ['DETAILS']['msg'],
-                         duration= config['DETAILS']['duration'],
-                         icon= config['DETAILS']['icon'])
+    toast = Notification(app_id=config['DETAILS']['appid'],
+                         title=config['DETAILS']['title'],
+                         msg=config['DETAILS']['msg'],
+                         duration=config['DETAILS']['length'],
+                         icon=config['DETAILS']['icon'])
     
     toast.set_audio(audio.IM, loop=False)
     
